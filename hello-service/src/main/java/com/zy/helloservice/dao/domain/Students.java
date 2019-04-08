46f36e23d503a73bdc38a648913e7eec44620b3c
zy <1210899298@qq.com>
2019-04-08 22:41:23 +0800

    添加MP的一些配置

--- hello-service/src/main/java/com/zy/helloservice/dao/domain/Students.java
+++ hello-service/src/main/java/com/zy/helloservice/dao/domain/Students.java
@@ -1,14 +1,15 @@
 package com.zy.helloservice.dao.domain;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.sun.tools.javah.Util;
 import lombok.Builder;
+import lombok.Data;
+
+import java.util.Date;
 
 @Builder
-@TableName("students")
+@Data
 public class Students {
-    @TableId(type = IdType.AUTO)
     private Integer id;
 
     private String name;
@@ -21,51 +22,12 @@ public class Students {
 
     private Integer teacher;
 
-    public Integer getId() {
-        return id;
-    }
+    private Date createtime;
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    private String lastName;
 
-    public String getName() {
-        return name;
-    }
+    @TableField(exist = false)
+    private String fatherName;
 
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address == null ? null : address.trim();
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex == null ? null : sex.trim();
-    }
-
-    public Integer getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(Integer teacher) {
-        this.teacher = teacher;
-    }
 }
--- hello-service/src/test/java/com/zy/helloservice/HelloServiceApplicationTests.java
+++ hello-service/src/test/java/com/zy/helloservice/HelloServiceApplicationTests.java
@@ -22,18 +22,19 @@ public class HelloServiceApplicationTests {
     @Test
     public void tsetmapper() {
         Students students = studentsMapper.selectById(3);
+        logger.debug("debug");
         logger.info("info");
         logger.warn("warn");
-        logger.debug("debug");
         logger.error("error");
-        System.out.println(students.getAddress());
+        System.out.println(students.getLastName());
     }
 
     @Test
     public void testinsert() {
-        Students students = Students.builder().address("湖北").sex("男").age(23).name("zpl").teacher(2).build();
+        Students students = Students.builder().address("湖北").sex("男").age(23).name("zpl").teacher(2).lastName("厉害了").fatherName("呵呵哒").build();
+        logger.info(students.toString());
         int insert = studentsMapper.insert(students);
         System.out.println(insert);
-
+        System.out.println(students.getId());
     }
 }
