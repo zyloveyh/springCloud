6af562acb4508785e7757f14d2c083fa5d464969
zy <1210899298@qq.com>
2019-04-12 21:42:02 +0800

    新增服务采用最新的springboot

--- schoolrelation/src/main/java/com/zy/schoolrelation/mp/controller/StudentsController.java
+++ schoolrelation/src/main/java/com/zy/schoolrelation/mp/controller/StudentsController.java
@@ -1,20 +1,30 @@
 package com.zy.schoolrelation.mp.controller;
 
 
+import com.zy.schoolrelation.mp.entity.Students;
+import com.zy.schoolrelation.mp.service.impl.StudentsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author zy
  * @since 2019-04-10
  */
 @RestController
-@RequestMapping("/mp/students")
+@RequestMapping(value = "/mp/students",produces = "application/json;charset=UTF-8")
 public class StudentsController {
 
+    @Autowired
+    private StudentsServiceImpl studentsService;
+
+    @RequestMapping("getByKey")
+    public Students getByKey(String key) {
+        return studentsService.getById(key);
+    }
 }
