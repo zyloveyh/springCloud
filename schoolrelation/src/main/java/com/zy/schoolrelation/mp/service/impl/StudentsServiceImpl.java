f4470ebbb5d5b3249a3ef8f4c95a4a4af67bdb47
zy <1210899298@qq.com>
2019-05-14 22:40:14 +0800

    多数据源练习

--- schoolrelation/src/main/java/com/zy/schoolrelation/mp/service/impl/StudentsServiceImpl.java
+++ schoolrelation/src/main/java/com/zy/schoolrelation/mp/service/impl/StudentsServiceImpl.java
@@ -1,9 +1,11 @@
 package com.zy.schoolrelation.mp.service.impl;
 
+import com.baomidou.dynamic.datasource.annotation.DS;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zy.schoolrelation.mp.entity.Students;
 import com.zy.schoolrelation.mp.mapper.StudentsMapper;
 import com.zy.schoolrelation.mp.service.IStudentsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +19,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class StudentsServiceImpl extends ServiceImpl<StudentsMapper, Students> implements IStudentsService {
 
+    @Autowired
+    private StudentsMapper studentsMapper;
+
+    @DS("slave_1")
+    public Students getById1(String key) {
+        return studentsMapper.selectById(key);
+    }
+
+    @DS("slave_2")
+    public Students getById2(String key) {
+        return studentsMapper.selectById(key);
+    }
 }
